630dd7bfe1a1d50be5979ddca111573416e925b3
Fabian Flugund <fabian.flugund@gmail.com>
2017-10-14 17:17:29 +0200

    Hello world

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getVoteSubjects = functions.https.onRequest((request, response) => {
+  const data = [
+    {
+      "userId": "id",
+      "imageUrl": "url1"
+    },
+    {
+      "userId": "id",
+      "imageUrl": "url1"
+    },
+    {
+      "userId": "id",
+      "imageUrl": "url1"
+    }
+  ];
+
+  response.send(data);
+});
