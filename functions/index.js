d96c18604f3c84ea59c5ec1d1e6a7dbd257d01f7
Fabian Flugund <fabian.flugund@gmail.com>
2017-10-14 18:26:12 +0200

    LOad data from databse

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,12 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
 exports.getVoteSubjects = functions.https.onRequest((request, response) => {
-  const data = [
-    {
-      "userId": "1",
-      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/3/37/Sus_Barbatus%2C_the_Bornean_Bearded_Pig_%2812616351323%29.jpg"
-    },
-    {
-      "userId": "2",
-      "imageUrl": "http://i3.mirror.co.uk/incoming/article10109983.ece/ALTERNATES/s615/Blue-rat.jpg"
-    }
-  ];
+  const userId = request.query.userId;
 
-  response.send(data);
+  admin.database().ref('/users').once('value').then(users => {
+    response.send(users);
+  });
 });
