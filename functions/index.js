51e3c11210fba6f8bad0e5d4935cf9fc2eebbdeb
Fabian Flugund <fabian.flugund@gmail.com>
2017-10-14 17:33:49 +0200

    Send mot data to getVoteSubjects

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,12 @@ const functions = require('firebase-functions');
 exports.getVoteSubjects = functions.https.onRequest((request, response) => {
   const data = [
     {
-      "userId": "id",
-      "imageUrl": "url1"
+      "userId": "1",
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/3/37/Sus_Barbatus%2C_the_Bornean_Bearded_Pig_%2812616351323%29.jpg"
     },
     {
-      "userId": "id",
-      "imageUrl": "url1"
-    },
-    {
-      "userId": "id",
-      "imageUrl": "url1"
+      "userId": "2",
+      "imageUrl": "http://i3.mirror.co.uk/incoming/article10109983.ece/ALTERNATES/s615/Blue-rat.jpg"
     }
   ];
 
