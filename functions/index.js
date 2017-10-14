6bdbc94a81124fa60c4e6e9284a31ad0310aacd0
agentbellnorm <morgan.bentell@gmail.com>
2017-10-14 22:31:12 +0200

    no need to pass admin to services

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,19 @@ const admin = require('firebase-admin');
 
 const castVote = require('./services/cast-vote.js');
 const getVoteSubjects = require('./services/get-vote-subjects.js');
+const getNumberOfVotes = require('./services/get-number-of-votes.js');
 
 admin.initializeApp(functions.config().firebase);
 
 exports.getVoteSubjects = functions.https.onRequest((request, response) => 
-  	getVoteSubjects(request, response, admin)
+  	getVoteSubjects(request, response)
 );
 
 exports.castVote = functions.https.onRequest((request, response) => 
-	castVote(request, response, admin)
+	castVote(request, response)
+);
+
+exports.getNumberOfVotes = functions.https.onRequest((request, response) => 
+	castVote(request, response)
 );
 
