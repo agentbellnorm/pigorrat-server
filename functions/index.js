12e4d195ae31af9bac8121e82dcbfb0606ad919f
Fabian Flugund <fabian.flugund@gmail.com>
2017-10-14 21:52:19 +0200

    List vote subjects

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,37 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const omit = require('lodash/omit');
+const keys = require('lodash/keys');
+const concat = require('lodash/concat');
+const transform = require('lodash/transform');
+
 admin.initializeApp(functions.config().firebase);
 
 exports.getVoteSubjects = functions.https.onRequest((request, response) => {
   const userId = request.query.userId;
 
-  admin.database().ref('/users').once('value').then(users => {
-    response.send(users);
+  if (!userId) {
+    throw new Error("UserId is required as query parameter.");
+  }
+
+  Promise.all([
+    admin.database().ref('/votes/by/'+ userId).once('value'),
+    admin.database().ref('/users/').limitToLast(50).once('value')
+  ]).then(values => {
+    const votes = values[0].val();
+    const users = values[1].val();
+
+    const omittedUsers = omit(users, concat(keys(votes), userId));
+
+    const transformedUsers = transform(omittedUsers, (result, user, key) => {
+      result.push({
+        'userId' : key,
+        'name' : user.name,
+        'imgUrl' : user.imgUrl
+      });
+    }, []);
+
+    response.send(transformedUsers);
   });
 });
